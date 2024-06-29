a55d2810489f740cfffb57dfac928c3c6488af21
awantika <tiwariawantika13@gmail.com>
2024-06-29 22:29:56 +0530

    react app created --- navbar completed and header created

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+import Navbar from "../../components/Navbar/navbar";
+import Header from "../../components/Header/header";
+const Home = () => {
+    return ( 
+        <div>
+            <Navbar></Navbar>
+            <Header></Header>
+        </div>
+     );
+} 
+ 
+export default Home;
